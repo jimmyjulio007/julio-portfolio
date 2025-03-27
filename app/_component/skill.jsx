14bc155ec3a0509a7b4252bdb93b598b2db62f01
jimmyjulio007 <jimmyjulio100@gmail.com>
2025-03-27 20:54:13 +0300

    alt correction wit gitlab and redis icon add

--- app/_component/skill.jsx
+++ app/_component/skill.jsx
@@ -54,15 +54,16 @@ export default function Skill() {
                     type: 'tween'
                 }}
                  className='grid grid-cols-4 lg:grid-cols-5 flex-wrap items-center gap-3 md:gap-4'>
-                    <Image src={'/icon/nodejs.svg'} alt='HTML' width={75} height={45} />
-                    <Image src={'/icon/nestjs.svg'} alt='CSS' width={75} height={45} />
-                    <Image src={'/icon/firebase.svg'} alt='JS' width={75} height={50} />
-                    <Image src={'/icon/supabase.svg'} alt='JS' width={75} height={50} />
-                    <Image src={'/icon/graphql.svg'} alt='JS' width={75} height={50} />
-                    <Image src={'/icon/postgresql.svg'} alt='TS' width={75} height={45} />
-                    <Image src={'/icon/mysql.svg'} alt='REACT' width={75} height={45} />
-                    <Image src={'/icon/auth0.svg'} alt='NEXT' width={75} height={45} />
-                    <Image src={'/icon/mongodb.svg'} alt='REACT' width={35} height={20} />
+                    <Image src={'/icon/nodejs.svg'} alt='nodejs' width={75} height={45} />
+                    <Image src={'/icon/nestjs.svg'} alt='nestjs' width={75} height={45} />
+                    <Image src={'/icon/firebase.svg'} alt='firebase' width={75} height={50} />
+                    <Image src={'/icon/supabase.svg'} alt='supabase' width={75} height={50} />
+                    <Image src={'/icon/graphql.svg'} alt='graphql' width={75} height={50} />
+                    <Image src={'/icon/redis.svg'} alt='redis' width={75} height={20} />
+                    <Image src={'/icon/postgresql.svg'} alt='Postgresql' width={75} height={45} />
+                    <Image src={'/icon/mysql.svg'} alt='mysql' width={75} height={45} />
+                    <Image src={'/icon/auth0.svg'} alt='auth0' width={75} height={45} />
+                    <Image src={'/icon/mongodb.svg'} alt='mongo' width={35} height={20} />
                 </motion.div>
             </div>
             <div className='flex flex-col justify-start items-center gap-4'>
@@ -77,13 +78,14 @@ export default function Skill() {
                 type: 'tween'
             }}
              className='grid grid-cols-4 lg:grid-cols-5 flex-wrap items-center gap-3 md:gap-4'>
-                    <Image src={'/icon/vscode.svg'} alt='HTML' width={75} height={45} />
-                    <Image src={'/icon/figma.svg'} alt='REACT' width={75} height={45} />
-                    <Image src={'/icon/postman.svg'} alt='CSS' width={75} height={45} />
-                    <Image src={'/icon/openai.svg'} alt='JS' width={75} height={50} />
-                    <Image src={'/icon/git.svg'} alt='JS' width={75} height={50} />
-                    <Image src={'/icon/github.svg'} alt='JS' width={75} height={50} />
-                    <Image src={'/icon/bitbucket.svg'} alt='TS' width={75} height={45} />
+                    <Image src={'/icon/vscode.svg'} alt='vscode' width={75} height={45} />
+                    <Image src={'/icon/figma.svg'} alt='figma' width={75} height={45} />
+                    <Image src={'/icon/postman.svg'} alt='postamn' width={75} height={45} />
+                    <Image src={'/icon/openai.svg'} alt='openai' width={75} height={50} />
+                    <Image src={'/icon/gitlab.svg'} alt='gitlab' width={75} height={45} />
+                    <Image src={'/icon/git.svg'} alt='git' width={75} height={50} />
+                    <Image src={'/icon/github.svg'} alt='github' width={75} height={50} />
+                    <Image src={'/icon/bitbucket.svg'} alt='bitbucket' width={75} height={45} />
             </motion.div>
             </div>
             <div className='flex flex-col justify-start items-center gap-4 md:gap-4'>
@@ -98,9 +100,9 @@ export default function Skill() {
                 type: 'tween'
             }}
             className='flex flex-wrap items-center gap-3'>
-                    <Image src={'/icon/jest.svg'} alt='HTML' width={75} height={45} />
-                    <Image src={'/icon/prettier.svg'} alt='REACT' width={75} height={45} />
-                    <Image src={'/icon/eslint.svg'} alt='CSS' width={75} height={45} />
+                    <Image src={'/icon/jest.svg'} alt='jest' width={75} height={45} />
+                    <Image src={'/icon/prettier.svg'} alt='prettier' width={75} height={45} />
+                    <Image src={'/icon/eslint.svg'} alt='eslint' width={75} height={45} />
             </motion.div>
             </div>
         </div>
