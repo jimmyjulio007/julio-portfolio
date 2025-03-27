227dc8d52cbd29f5f0b6225b32f72428cdb8a381
jimmyjulio007 <jimmyjulio100@gmail.com>
2025-03-27 20:35:56 +0300

    animation section about and skills

--- app/_component/about.jsx
+++ app/_component/about.jsx
@@ -3,6 +3,8 @@
 import { Download} from 'lucide-react'
 import Image from 'next/image'
 import React, { Suspense } from 'react'
+import * as motion from 'motion/react-client'
+
 
 export default function About() {
   return (
@@ -19,7 +21,18 @@ export default function About() {
         </div>
         <div className="relative max-md:px-4 max-sm:pb-4">
           <Suspense fallback={<div>Loading...</div>}>
+          <motion.div
+          initial={{opacity:0, x: 200, scale:0.2}}
+          whileInView={{opacity:1 ,x: 0,scale:1}}
+          exit={{opacity: 0, scale:0.5}}
+          transition={{
+              duration: 0.6,
+              delay: 0.4,
+              ease: 'easeInOut',
+          }}
+          >
           <Image loading='lazy' src="/jimmy.jpg" alt="jimmy" width={454} className="max-md:w-full rounded-[30px] object-contain" height={500}/>
+          </motion.div>
           </Suspense>
         <div className="absolute  -top-4 right-4 md:-top-3 md:-right-4 w-28 h-[34px] border-2 border-orange-500 opacity-50 rounded-[30px]"></div>
         <div className="absolute  bottom-16 left-0.5 md:-left-5 w-9 md:w-12 h-[178px] border-2 border-orange-500 opacity-50 rounded-[30px]"></div>
--- app/_component/skill.jsx
+++ app/_component/skill.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import * as motion from "motion/react-client"
 
 
 export default function Skill() {
@@ -12,7 +13,16 @@ export default function Skill() {
         <div className='grid md:grid-cols-2 gap-10 md:gap-12'>
             <div className='flex flex-col justify-start items-center gap-4'>
                 <h2 className='font-syne font-bold text-2xl'>Front-End</h2>
-                <div className='grid grid-cols-4 lg:grid-cols-5 flex-wrap items-center gap-3 md:gap-4'>
+                <motion.div 
+                initial={{opacity:0,x:-200, scale:0.2}}
+                whileInView={{opacity:1,x:0,scale:1}}
+                transition={{
+                    duration: 0.6,
+                    delay: 0.4,
+                    ease: [0, 0.71, 0.2, 1.01],
+                    type: 'tween'
+                }}
+                className='grid grid-cols-4 lg:grid-cols-5 flex-wrap items-center gap-3 md:gap-4'>
                     <Image src={'/icon/html.svg'} alt='HTML' width={75} height={45} />
                     <Image src={'/icon/css.svg'} alt='CSS' width={75} height={45} />
                     <Image src={'/icon/tailwind.svg'} alt='JS' width={75} height={50} />
@@ -30,11 +40,20 @@ export default function Skill() {
                     <Image src={'/icon/reactquery.svg'} alt='react query' width={75} height={45} />
                     <Image src={'/icon/zod.svg'} alt='zod' width={75} height={45} />
                     <Image src={'/icon/resend.svg'} alt='resend' width={75} height={45} />
-                </div>
+                </motion.div>
             </div>
             <div className='flex flex-col justify-start items-start gap-4'>
                 <h2 className='font-syne font-bold text-2xl'>Back-End & Database</h2>
-                <div className='grid grid-cols-4 lg:grid-cols-5 flex-wrap items-center gap-3 md:gap-4'>
+                <motion.div
+                initial={{opacity:0,x:200, scale:0.2}}
+                whileInView={{opacity:1,x:0,scale:1}}
+                transition={{
+                    duration: 1.2,
+                    delay: 0.4,
+                    ease: [0, 0.71, 0.2, 1.01],
+                    type: 'tween'
+                }}
+                 className='grid grid-cols-4 lg:grid-cols-5 flex-wrap items-center gap-3 md:gap-4'>
                     <Image src={'/icon/nodejs.svg'} alt='HTML' width={75} height={45} />
                     <Image src={'/icon/nestjs.svg'} alt='CSS' width={75} height={45} />
                     <Image src={'/icon/firebase.svg'} alt='JS' width={75} height={50} />
@@ -44,11 +63,20 @@ export default function Skill() {
                     <Image src={'/icon/mysql.svg'} alt='REACT' width={75} height={45} />
                     <Image src={'/icon/auth0.svg'} alt='NEXT' width={75} height={45} />
                     <Image src={'/icon/mongodb.svg'} alt='REACT' width={35} height={20} />
-                </div>
+                </motion.div>
             </div>
             <div className='flex flex-col justify-start items-center gap-4'>
             <h2 className='font-syne font-bold text-2xl'>Tools</h2>
-            <div className='grid grid-cols-4 lg:grid-cols-5 flex-wrap items-center gap-3 md:gap-4'>
+            <motion.div
+            initial={{opacity:0,x:-200, scale:0.2}}
+            whileInView={{opacity:1,x:0,scale:1}}
+            transition={{
+                duration: 1.5,
+                delay: 0.4,
+                ease: [0, 0.71, 0.2, 1.01],
+                type: 'tween'
+            }}
+             className='grid grid-cols-4 lg:grid-cols-5 flex-wrap items-center gap-3 md:gap-4'>
                     <Image src={'/icon/vscode.svg'} alt='HTML' width={75} height={45} />
                     <Image src={'/icon/figma.svg'} alt='REACT' width={75} height={45} />
                     <Image src={'/icon/postman.svg'} alt='CSS' width={75} height={45} />
@@ -56,15 +84,24 @@ export default function Skill() {
                     <Image src={'/icon/git.svg'} alt='JS' width={75} height={50} />
                     <Image src={'/icon/github.svg'} alt='JS' width={75} height={50} />
                     <Image src={'/icon/bitbucket.svg'} alt='TS' width={75} height={45} />
-            </div>
+            </motion.div>
             </div>
             <div className='flex flex-col justify-start items-center gap-4 md:gap-4'>
             <h2 className='font-syne font-bold text-2xl'>Test & Quality</h2>
-            <div className='flex flex-wrap items-center gap-3'>
+            <motion.div
+            initial={{opacity:0,x:200, scale:0.2}}
+            whileInView={{opacity:1,x:0,scale:1}}
+            transition={{
+                duration: 1.9,
+                delay: 0.4,
+                ease: [0, 0.71, 0.2, 1.01],
+                type: 'tween'
+            }}
+            className='flex flex-wrap items-center gap-3'>
                     <Image src={'/icon/jest.svg'} alt='HTML' width={75} height={45} />
                     <Image src={'/icon/prettier.svg'} alt='REACT' width={75} height={45} />
                     <Image src={'/icon/eslint.svg'} alt='CSS' width={75} height={45} />
-            </div>
+            </motion.div>
             </div>
         </div>
     </div>
