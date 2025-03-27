925265cae0abaf56867fdf0f739cb27df50a6441
jimmyjulio007 <jimmyjulio100@gmail.com>
2025-03-27 18:45:20 +0300

    Add speed insights

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Preloader from "@/components/preloader";
 import "./globals.css";
 import SmoothScrollProvider from "@/components/smooth";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export const metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
       >
         <Analytics />
         <Preloader />
+        <SpeedInsights />
         {children}
       </body>
     </SmoothScrollProvider>
